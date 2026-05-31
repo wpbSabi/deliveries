4bac2200a1a8809bd77fb333275fa975ee5216a8
Sabi Horvat <sabolch.horvat@gmail.com>
2026-05-31 14:31:49 -0700

    added train_test_split()

--- functions.py
+++ functions.py
@@ -33,6 +33,7 @@ def summarize_results(
     y_test: pd.Series,
     y_pred: pd.Series,
     model_name: str,
+    print_stats: bool = True,
 ) -> pd.DataFrame:
     """
     Compute metrics and return a results DataFrame for the predictions.
@@ -54,8 +55,10 @@ def summarize_results(
     r2 = r2_score(y_test, y_pred)
     mape = mean_absolute_percentage_error(y_test, y_pred)
 
-    print(f"Predicted rows: {len(y_pred)}")
-    print(f" MAE: {mae:.3f} \n RMSE: {rmse:.3f} \n R2: {r2:.3f} \n MAPE: {mape:.3f}")
+    # If print_stats is True, print the metrics to the console
+    if print_stats:
+        print(f"Predicted rows: {len(y_pred)}")
+        print(f" MAE: {mae:.3f} \n RMSE: {rmse:.3f} \n R2: {r2:.3f} \n MAPE: {mape:.3f}")
 
     # Create a results DataFrame that includes the dimensions and the actual vs predicted metrics
     results = (
@@ -72,4 +75,47 @@ def summarize_results(
     results["abs_err"] = (results["actual"] - results["pred"]).abs()
     results["model"] = model_name
 
-    return results
+    return results, mape
+
+
+def train_test_split(df: pd.DataFrame, run_date: pd.Timestamp) -> tuple:
+    """
+    Split the DataFrame into training and testing sets based on the order_date.
+    The testing data will include one week of data starting from the run_date,
+        and the training data will include all data up to the run_date.
+
+    Args:
+        df (pd.DataFrame): The input DataFrame containing the features data to be split.
+        run_date (pd.Timestamp): The date to use as the cutoff for splitting the data.
+
+    Returns:
+        tuple: A tuple containing the training features (X_train), training target (y_train),
+               testing features (X_test), and testing target (y_test).
+    """
+    # Training data to include all data up to the run_date,
+    # and testing data to include all data from the run_date to one week after the run_date
+    features_train = df[df["order_date"] < run_date]
+    features_test1 = df[df["order_date"] >= run_date]
+    features_test = features_test1[
+        features_test1["order_date"] <= run_date + pd.Timedelta(days=7)
+    ]
+
+    # Remove order date from features, maybe add back for future features, such as a rolling average or seasonality
+    features_train = features_train.drop(columns=["order_date", "order_number"])
+    features_test = features_test.drop(columns=["order_date", "order_number"])
+
+    features_train.reset_index(drop=True, inplace=True)
+    features_test.reset_index(drop=True, inplace=True)
+
+    # drop non-feature columns, and move the target actual_days to the last columns
+    feature_cols = [col for col in features_train.columns if col != "actual_days"]
+    features_train = features_train[feature_cols + ["actual_days"]]
+    features_train.reset_index(drop=True, inplace=True)
+
+    # Create training and testing variables
+    X_train = features_train[feature_cols]
+    y_train = features_train["actual_days"]
+    X_test = features_test[feature_cols]
+    y_test = features_test["actual_days"]
+
+    return X_train, y_train, X_test, y_test, feature_cols
